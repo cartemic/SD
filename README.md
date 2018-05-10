6bdcce060862c2a01928bb92edcd28a2d386f4e5
Carter <cartemic@oregonstate.edu>
2018-05-10 14:25:28 -0700

    pip deployment prep

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # SD2: A SDToolbox fork for python <img src="https://travis-ci.org/cartemic/SD2.svg?branch=master">
-I am working on making Caltech's SDToolbox more readable, PEP 8 compliant, and python 3 compatible, as well as adding in unit tests. I will be using this package for use in my doctoral research project involving gaseous detonations, in conjunction with <a href="https://github.com/cartemic/BeaverDet">BeaverDet</a> (currently under development). Where possible, I am replacing their functions with numpy and scipy equivalents. So far this has improved CJ speed calculation time by ~20%.
 
-Original files from [here](http://shepherd.caltech.edu/EDL/public/cantera/html/SD_Toolbox/)
+This package is a modified fork of [SDToolbox](http://shepherd.caltech.edu/EDL/public/cantera/html/SD_Toolbox/), and all credit for the original functions goes to the researchers in Caltech's [Explosion Dynamics Laboratory](http://shepherd.caltech.edu/EDL/). The functions in this package have been modified and moved around with the intent of making the package faster, more readable, PEP 8 compliant, and deployable using pip/conda.
+
+\- [Mick Carter](mailto:cartemic@oregonstate.edu), 10 May 2018
---
+++ setup.py
@@ -0,0 +1,64 @@
+from codecs import open
+from os import path
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'sd2', '_version.py')) as version_file:
+    exec(version_file.read())
+
+with open(path.join(here, 'README.md')) as readme_file:
+    readme = readme_file.read()
+
+with open(path.join(here, 'CHANGELOG.md')) as changelog_file:
+    changelog = changelog_file.read()
+
+desc = readme + '\n\n' + changelog
+try:
+    import pypandoc
+    long_description = pypandoc.convert_text(desc, 'rst', format='md')
+    with open(path.join(here, 'README.rst'), 'w') as rst_readme:
+        rst_readme.write(long_description)
+except (ImportError, OSError, IOError):
+    long_description = desc
+
+install_requires = [
+    'numpy',
+]
+
+tests_require = [
+    'pytest',
+    'pytest-cov',
+]
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+setup_requires = ['pytest-runner'] if needs_pytest else []
+
+setup(
+    name='sd2',
+    version='0.1.0',
+    description='A SDToolbox fork for python',
+    author='Mick Carter',
+    author_email='cartemic@oregonstate.edu',
+    url='https://github.com/cartemic/SD2',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+    license='BSD-3-Clause',
+    python_requires='>=2.7.*, >=3.5.*',
+    zip_safe=False,
+    package=find_packages(),
+    include_package_data=True,
+)
+
+if __name__ == "__main__":
+    setup()
